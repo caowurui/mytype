90eeb9d5822949ebe824bff1a9dfc32694df87ed
caowurui <caowurui@qq.com>
2025-07-28 19:33:23 +0800

    增加得分模块，可以查看得分情况。 实现字体居中。

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ const int WinWidth = 480;               //窗口宽度
 const int WinHeight = 600;              //窗口高度
 const int RedLine = 400;                //红线所在位置（纵坐标）
 const int RedLineWidth = 4;             //红线高度
+const Color BackgroundColor = LIGHTGRAY;//背景颜色
 
 const int letter_rate = 100;            //字母生成速率
 const int letter_v = 2;                 //字母下落速率
@@ -12,10 +13,15 @@ const int letter_v = 2;                 //字母下落速率
 const int font_size = 30;               //字母大小
 const float spacing = 0.0f;             //字母间距
 const char font_name[] =
-        "C:/Windows/Fonts/consola.ttf"; //字体文件路径
+        "C:/Windows/Fonts/simhei.ttf";  //字体文件路径
+const char characters[]=
+        "abcdefghijklmnopqrstuvwxyzENPRT0123456789.:[]最高分数";
+                                        //字符集
 Font font;                              //字体资源文件
 
 bool GameOver = true;                   //游戏是否中止
+int point = 0;                          //分数
+int max_point = 0;                      //最高分数
 
 struct aLetter{
     int x,y;
@@ -53,7 +59,7 @@ void GetFont(){
     int filesize;
     unsigned char* fileData=LoadFileData(font_name,&filesize);
     int cnt;
-    int* codepoints=LoadCodepoints("abcdefghijklmnopqrstuvwxyzENPRT.[]",&cnt);
+    int* codepoints=LoadCodepoints(characters,&cnt);
     font=LoadFontFromMemory(".ttf",fileData,filesize,font_size,codepoints,cnt);
     UnloadCodepoints(codepoints);
     UnloadFileData(fileData);
@@ -68,9 +74,12 @@ void Process(){
 
 //按到对应字母后消除
     for(int i=65;i<=90;i++)
-        if(IsKeyPressed(i)){
-            letters[i-65].clear();
-        }
+        if(IsKeyPressed(i))
+            if(!letters[i-65].empty()){
+                point++;
+                if(point>=max_point)max_point=point;
+                letters[i-65].clear();
+            }
 
 //字母整体位移
     for(int i=0;i<26;i++)
@@ -94,7 +103,7 @@ void GenerateLetter(){
 void Draw(){
     BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(BackgroundColor);
 
         DrawRectangle(0,RedLine,WinWidth,RedLineWidth,RED);
 
@@ -103,11 +112,16 @@ void Draw(){
                 DrawTextEx(font,TextFormat("%c",i+'a'),Vector2{(float)it.x,(float)it.y},font_size,spacing,BLACK);
             }
 
+        Vector2 tmp=MeasureTextEx(font,TextFormat("分数:%d",point),font_size,spacing);
+        DrawTextEx(font,TextFormat("分数:%d",point),Vector2{(WinWidth-tmp.x)/2.0f,0.0f},font_size,spacing,SKYBLUE);
+        DrawTextEx(font,TextFormat("最高分数:%d",max_point),Vector2{(WinWidth-MeasureTextEx(font,TextFormat("最高分数:%d",max_point),font_size,spacing).x)/2.0f,tmp.y},font_size,spacing,MAROON);
+
     EndDrawing();
 }
 
 void ToBeContinued(){
     if(IsKeyPressed(KEY_ENTER)){
+        point=0;
         GameOver=false;
         for(int i=0;i<26;i++)
             letters[i].clear();
@@ -115,10 +129,11 @@ void ToBeContinued(){
 
     BeginDrawing();
 
-        ClearBackground(RAYWHITE);
+        ClearBackground(BackgroundColor);
         DrawRectangle(0,RedLine,WinWidth,RedLineWidth,RED);
 
-        DrawTextEx(font,"Press [ENTER] to continue.",Vector2{0,WinHeight/2},font_size,spacing,BLACK);
+        Vector2 tmp=MeasureTextEx(font,"Press [ENTER] to continue.",font_size,spacing);
+        DrawTextEx(font,"Press [ENTER] to continue.",Vector2{(WinWidth-tmp.x)/2.0f,RedLine-font_size},font_size,spacing,DARKBROWN);
 
     EndDrawing();
 }
